995c89892c0ec808524cb15196a29f60e526dc6a
Eve Le <nguyenthao.lee@gmail.com>
2019-09-04 16:45:07 -0700

    Implement constructor and readers and add tests for HotelSystem class

--- lib/hotel_system.rb
+++ lib/hotel_system.rb
@@ -1,5 +1,20 @@
 module Hotel
     class HotelSystem
-        
+        RATE = 200.00
+        NUMBER_OF_ROOM = 20
+
+        attr_reader :number_of_rooms
+
+        def initialize(number_of_room = NUMBER_OF_ROOM)
+            @data = Hash.new
+            number_of_room.times do |index|
+                @data[(index + 1)] = Array.new
+            end
+            @number_of_rooms = number_of_room
+        end
+
+        def reservations
+            return @data.values.flatten
+        end
     end
 end
--- test/hotel_system_test.rb
+++ test/hotel_system_test.rb
@@ -6,5 +6,16 @@ describe Hotel::HotelSystem do
             hotel_system = Hotel::HotelSystem.new()
             expect (hotel_system).must_be_instance_of Hotel::HotelSystem
         end
+
+        it "can instantiate a HotelSystem that tracks 20 rooms" do
+            expected_number_of_rooms = 20
+            hotel_system = Hotel::HotelSystem.new()
+            expect (hotel_system.number_of_rooms).must_equal expected_number_of_rooms
+        end
+
+        it "allows user to access a list of Reservation" do
+            hotel_system = Hotel::HotelSystem.new()
+            expect (hotel_system.reservations).must_be_instance_of Array
+        end
     end
 end
