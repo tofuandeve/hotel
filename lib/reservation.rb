534f6471b1ce4fe324a1fb7eaebc4d931acd24a7
Eve Le <nguyenthao.lee@gmail.com>
2019-09-03 17:38:19 -0700

    Add @cost as a instance variable to Reservation class and update test for it

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,11 +1,12 @@
 module Hotel
     class Reservation
-        attr_reader :id, :date_range, :rate 
-
+        attr_reader :id, :date_range, :rate, :cost
+        
         def initialize(date_range, rate, id)
             @date_range = date_range
             @rate = rate
             @id = id
+            @cost = (date_range.start_date - date_range.end_date).abs * rate
         end
     end
 end
--- test/reservation_test.rb
+++ test/reservation_test.rb
@@ -8,9 +8,13 @@ describe Hotel::Reservation do
             date_range = Hotel::DateRange.new(start_date, end_date)
             rate = 200.00
             id = 1000
-
+            expected_cost = 3 * rate
+            
             reservation = Hotel::Reservation.new(date_range, rate, id)
             expect (reservation).must_be_instance_of Hotel::Reservation
+            expect (reservation.date_range).must_equal date_range
+            expect (reservation.id).must_equal id
+            expect (reservation.cost).must_equal expected_cost
         end
-    end
+    end  
 end
