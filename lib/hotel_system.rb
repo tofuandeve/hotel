d906e6e2b743c04242d913a849767cfdeec94f70
Eve Le <nguyenthao.lee@gmail.com>
2019-09-04 17:19:27 -0700

    Implement make_reservation and reservation_ids methods for HotelSystem class and add tests

--- lib/hotel_system.rb
+++ lib/hotel_system.rb
@@ -2,19 +2,36 @@ module Hotel
     class HotelSystem
         RATE = 200.00
         NUMBER_OF_ROOM = 20
-
+        
         attr_reader :number_of_rooms
-
+        
         def initialize(number_of_room = NUMBER_OF_ROOM)
-            @data = Hash.new
+            @data = Hash.new   
             number_of_room.times do |index|
                 @data[(index + 1)] = Array.new
             end
             @number_of_rooms = number_of_room
         end
-
+        
         def reservations
             return @data.values.flatten
+        end
+        
+        def reservation_ids
+            return reservations.map { |reservation| reservation.id }
+        end
+        
+        def make_reservation(start_date, end_date)
+            available_room = @data.find { |number, reservations| reservations.empty? }
+            if available_room
+                date_range = DateRange.new(start_date, end_date)
+                
+                reservation_id = rand(1000..9999)
+                while reservation_ids.include? reservation_id
+                    reservation_id = rand(1000..9999)
+                end
+                @data[available_room[0]] << Reservation.new(date_range, RATE, reservation_id)
+            end
         end
     end
 end
--- test/hotel_system_test.rb
+++ test/hotel_system_test.rb
@@ -18,4 +18,50 @@ describe Hotel::HotelSystem do
             expect (hotel_system.reservations).must_be_instance_of Array
         end
     end
+
+    describe "make reservation method" do
+        it "can update the list of reservations for valid date range" do
+            hotel_system = Hotel::HotelSystem.new()
+            start_date = '2019-09-10'
+            end_date = '2019-09-13'
+            number_of_reservations = hotel_system.reservations.length
+            expected_number_of_rooms = hotel_system.number_of_rooms
+
+            hotel_system.make_reservation(start_date, end_date)
+            expect (hotel_system.reservations.length).must_equal (number_of_reservations + 1)
+            expect (hotel_system.number_of_rooms).must_equal expected_number_of_rooms
+        end
+
+        it "does nothing if no rooms are available" do
+            hotel_system = Hotel::HotelSystem.new()
+            start_date = '2019-09-10'
+            end_date = '2019-09-13'
+            
+            expected_number_of_rooms = hotel_system.number_of_rooms
+
+            20.times do 
+                hotel_system.make_reservation(start_date, end_date)
+            end
+
+            number_of_reservations = hotel_system.reservations.length
+            expect (hotel_system.reservations.length).must_equal number_of_reservations
+            expect (hotel_system.number_of_rooms).must_equal expected_number_of_rooms
+
+            hotel_system.make_reservation(start_date, end_date)
+            expect (hotel_system.reservations.length).must_equal number_of_reservations
+        end
+
+        it "raises ArgumentError for invalid start and end dates" do
+            hotel_system = Hotel::HotelSystem.new()
+            start_date = '2019-09-10'
+            invalid_dates = ['2019-09-33','2019-19-23','2000-09-01']
+
+            number_of_reservations = hotel_system.reservations.length
+            number_of_rooms = hotel_system.number_of_rooms
+
+            invalid_dates.each do |invalid_date|
+                expect {hotel_system.make_reservation(start_date, invalid_date)}.must_raise ArgumentError
+            end
+        end
+    end
 end
