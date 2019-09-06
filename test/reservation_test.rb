9417e8135634d9133f6dbc26a37abac7e9ea8423
Eve Le <nguyenthao.lee@gmail.com>
2019-09-05 22:32:29 -0700

    Update tests for Reservation class

--- test/reservation_test.rb
+++ test/reservation_test.rb
@@ -8,13 +8,26 @@ describe Hotel::Reservation do
             date_range = Hotel::DateRange.new(start_date, end_date)
             rate = 200.00
             id = 1000
-            expected_cost = 3 * rate
             
             reservation = Hotel::Reservation.new(date_range, rate, id)
             expect (reservation).must_be_instance_of Hotel::Reservation
             expect (reservation.date_range).must_equal date_range
             expect (reservation.id).must_equal id
+        end
+    end
+    
+    describe "Reservation class property" do
+        it "doesn't charge checkout date for reservation's cost" do
+            start_date = '2019-09-10'
+            end_date = '2019-09-13'
+            date_range = Hotel::DateRange.new(start_date, end_date)
+            rate = 200.00
+            id = 1000
+            stay_duration = 3
+            expected_cost = stay_duration * rate
+            
+            reservation = Hotel::Reservation.new(date_range, rate, id)
             expect (reservation.cost).must_equal expected_cost
         end
-    end  
+    end
 end
